20eba85f7d6c88a527e89cf12b82cd6f8e10a556
klinkert0728 <dklinker@eafit.edu.co>
2017-10-19 09:44:21 -0500

    add user properties

--- StoreIOS/Model/DKHUser.swift
+++ StoreIOS/Model/DKHUser.swift
@@ -12,11 +12,41 @@ import RealmSwift
 
 class DKHUser: Object,Mappable {
     
+    dynamic var uuid        = ""
+    dynamic var name        = ""
+    dynamic var email       = ""
+    dynamic var userType    = ""
+    dynamic var token       = ""
+    
+    
+    class var currentUser:DKHUser? {
+        var user:DKHUser? = nil
+        Realm.update { (realm) in
+            user = realm.objects(DKHUser.self).first
+        }
+        return user
+    }
+    
+    class var isLoggedIn:Bool {
+        return currentUser != nil
+    }
+    
+    class func logOut() {
+        Realm.update { (realm) in
+            realm.delete(realm.objects(DKHUser.self))
+        }
+    }
     convenience required init?(map: Map) {
         self.init()
     }
     
     func mapping(map: Map) {
         
+        uuid        <- map["uuid"]
+        name        <- map["name"]
+        email       <- map["userType"]
+        token       <- map["token"]
     }
+    
+
 }
